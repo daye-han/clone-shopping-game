e4a85671cdb4b3e9c6236fa2adf648fc04fecb2d
daye-han <handot38@gmail.com>
2021-02-01 18:21:54 +0900

    items 전체 보여주기

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+// main
+loadItems()
+    .then(items => {
+        displayItems(items); //items 보여주기
+        // setEventListeners(items); //items 이벤트 걸기
+    })
+    .catch(console.log('error'));
+
+
+function loadItems() {
+    return fetch('../data/data.json')
+        .then(response => response.json())
+        .then(json => json.items)
+}
+
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item=> createHTMLString(item)).join('');
+}
+
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" class="item__thumbnail" alt="${item.type}">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
